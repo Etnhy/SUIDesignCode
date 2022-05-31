e1bbda47fd6356a100a102c960a23537cd6a279d
Evhenii Mahlena <magellan7777@icloud.com>
2022-05-31 23:33:24 +0300

    Initial Commit

---
+++ SUIDesignCode/SUIDesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIDesignCodeApp.swift
+//  SUIDesignCode
+//
+//  Created by Evhenii Mahlena on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIDesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
