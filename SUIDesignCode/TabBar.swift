115977e0b3c5477fc81d9597767214aad1642601
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-01 21:43:17 +0300

    tabbar +

---
+++ SUIDesignCode/TabBar.swift
@@ -0,0 +1,68 @@
+//
+//  TabBar.swift
+//  SUIDesignCode
+//
+//  Created by Evhenii Mahlena on 01.06.2022.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    
+    @State var selectedTab: Tab = .home
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            
+            Group {
+                switch selectedTab {
+                case .home:
+                    ContentView()
+                case .explore:
+                    AccountView()
+                case .notifications:
+                    AccountView()
+                case .library:
+                    AccountView()
+                }
+            }
+            .frame(maxWidth: . infinity, maxHeight: .infinity)
+
+            HStack {
+                ForEach(tabItems) { item in
+                    
+                    Button {
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 0) {
+                            Image(systemName: item.icon)
+                                .symbolVariant(.fill)
+                                .font(.body.bold())
+                                .frame(width: 44, height: 29)
+                            Text(item.text)
+                                .font(.caption2)
+                                .lineLimit(1)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+                } // v stack
+                
+            } // h stack
+            .padding(.horizontal, 8)
+            .padding(.top, 14)
+            .frame(height: 88, alignment: .top)
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .strokeStyle(34)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .ignoresSafeArea()
+            
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
