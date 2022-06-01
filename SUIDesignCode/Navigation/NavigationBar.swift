f9f97addc38ecdc5a356caeb4c01c65f5303cc87
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-01 22:58:40 +0300

    navigation bar + blur

---
+++ SUIDesignCode/Navigation/NavigationBar.swift
@@ -0,0 +1,34 @@
+//
+//  NavigationBar.swift
+//  SUIDesignCode
+//
+//  Created by Evhenii Mahlena on 01.06.2022.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    
+    var title = " "
+    
+    var body: some View {
+        ZStack {
+            Color.clear
+                .background(.ultraThinMaterial)
+                .blur(radius: 10)
+            Text(title)
+                .font(.largeTitle.weight(.bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+        }
+        .frame(height: 70)
+        .frame(maxHeight: .infinity, alignment: .top)
+
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar(title: "Featured")
+    }
+}
