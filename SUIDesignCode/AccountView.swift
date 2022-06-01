b8787c15817f8311df9aae78e249b9d5f4adb846
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-01 17:05:08 +0300

    refactor Account view

--- SUIDesignCode/AccountView.swift
+++ SUIDesignCode/AccountView.swift
@@ -14,90 +14,106 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.largeTitle)
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.pink)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                        )
-                    Text("Evhenii Mahlena")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.large)
-                        Text("Ukraine")
-                            .foregroundColor(.secondary)
-                    }
-                } // VSTACK
-                .frame(maxWidth: .infinity)
-                .padding()
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    
-                    NavigationLink { Text("Billing") } label:  {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    
-                    NavigationLink { ContentView() } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                    
-                } /* Section where: Settings,Billing,Help */
+                profile
+                
+                menu
+                
+                links
                 
                 .accentColor(.primary)
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
                 
-                Section {
-                    if !isDelete {
-                        Link(destination: URL(string: "https://www.apple.com")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading , allowsFullSwipe: true) {
-                            Button { isDelete = true } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            Button {
-                                //
-                            } label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                        }
-                    }
-                    Link(destination: URL(string: "https://www.youtube.com")!) {
-                        HStack {
-                            Label("YouTube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    
-                }   /* Section where "Website" */
                 .listRowSeparator(.hidden)
                 .accentColor(.primary)
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         } // Navigation View
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.largeTitle)
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.pink)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+                )
+            Text("Evhenii Mahlena")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.large)
+                Text("Ukraine")
+                    .foregroundColor(.secondary)
+            }
+        } // VSTACK
+        .frame(maxWidth: .infinity)
+        .padding()
+
+    }
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            
+            NavigationLink { Text("Billing") } label:  {
+                Label("Billing", systemImage: "creditcard")
+            }
+            
+            NavigationLink { ContentView() } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+            
+        } /* Section where: Settings,Billing,Help */
+
+    }
+    
+    var links: some View {
+        Section {
+            if !isDelete {
+                Link(destination: URL(string: "https://www.apple.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading , allowsFullSwipe: true) {
+                    Button { isDelete = true } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    Button {
+                        //
+                    } label: {
+                        Label("Pin", systemImage: "pin")
+                    }
+                    .tint(.yellow)
+                }
+            }
+            Link(destination: URL(string: "https://www.youtube.com")!) {
+                HStack {
+                    Label("YouTube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            
+        }   /* Section where "Website" */
+
     }
 }
 
