9966f6a4e3d713bebaf4374a6c2aad5df5c566df
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-10 13:26:27 +0300

    parallax and rotation 3d effect

--- SUIDesignCode/Components/FeaturedItem.swift
+++ SUIDesignCode/Components/FeaturedItem.swift
@@ -39,21 +39,15 @@ struct FeaturedItem: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .background(.ultraThinMaterial)
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+//        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .strokeStyle()
         .padding(.horizontal, 20)
 //        .background(
 //            Image("Blob 1")
 //                .offset(x: 250, y: -100)
 //        )
-        .overlay(
-            Image(course.image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 250)
-                .offset(x: 32, y: -100)
-        )
     }    
 }
 
